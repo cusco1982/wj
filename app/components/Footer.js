857258070212a4206cbd91a82444652b42f96f67
Julian <cusco2891@gmail.com>
2023-12-01 23:59:59 -0500

    update

---
+++ app/components/Footer.js
@@ -0,0 +1,28 @@
+
+
+// import Image from 'next/image';
+
+
+
+
+export default function Footer() {
+
+
+
+
+    return (
+        <div>
+            Footer
+
+
+
+
+
+                {/* <Typography py={1} style={{ color: '#40A5FE' }}>@ Mama's Cleaning NJ 2023</Typography> */}
+
+
+
+
+        </div>
+    )
+};
---
+++ app/components/Navbar.js
@@ -0,0 +1,21 @@
+// import Image from 'next/image';
+// import styles from './navbar.module.css';
+
+
+// const pages = ['home', 'services', 'gallery', 'contact'];
+
+
+
+
+export default function Navbar() {
+
+
+    return (
+        <div>
+
+            Navbar
+
+
+        </div>
+    )
+};
